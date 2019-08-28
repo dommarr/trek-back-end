2b5ba837bbee0f549aba16063cdc80a397c0c36d
dommarr <dom.marrone@gmail.com>
2019-08-27 22:20:12 -0400

    Update activity_serializer
    Updated activity_serializer to expose trip_id.

--- app/serializers/activity_serializer.rb
+++ app/serializers/activity_serializer.rb
@@ -1,5 +1,5 @@
 class ActivitySerializer < ActiveModel::Serializer
-  attributes :id, :begin_date, :end_date, :activity_title
+  attributes :id, :begin_date, :end_date, :activity_title, :trip_id
 
   def editable
     scope == object.user
